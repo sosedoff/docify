44348bed6daecc54d4a62efe9e82bbe4ffc4b6ab
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-05-23 16:43:25 -0500

    force utf8 in html

--- lib/docify/style.rb
+++ lib/docify/style.rb
@@ -5,6 +5,7 @@ REGEX = /(\{\{([a-z\-\_]{1,})\}\})/i
 <html>
   <head>
     <title>{{title}}</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
     {{css}}
   </head>
   <body>
