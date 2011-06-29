07c5266fef182849af3af20b174a3e3485edbf91
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-06-29 16:36:01 -0500

    added direct rendering feature

--- lib/docify.rb
+++ lib/docify.rb
@@ -4,3 +4,14 @@ require 'docify/template'
 require 'docify/style'
 require 'docify/markup'
 require 'docify/document'
+
+module Docify
+  # Simply renders content for the markup
+  def self.render(text, format='mardown')
+    if Docify::FORMATS.include?(format.to_s)
+      Docify::Markup.send(format.to_sym, text)
+    else
+      raise ArgumentError, "Invalid markup: #{format}."
+    end
+  end
+end
