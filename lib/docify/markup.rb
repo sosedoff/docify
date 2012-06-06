0e4b947b980325a4e9746a8044af43cc2238705c
Dan Sosedoff <dan.sosedoff@gmail.com>
2012-06-06 12:57:26 -0500

    Support flavored chunks

--- lib/docify/markup.rb
+++ lib/docify/markup.rb
@@ -9,17 +9,28 @@ module Docify
     # Render content for RDoc
     def rdoc(content)
       markup = RDoc::Markup::ToHtml.new
-      markup.convert(content)
+      markup.convert(process_content(content))
     end
     
     # Render content for Markdown
     def markdown(content)
-      Markdown.new(content).to_html
+      Markdown.new(process_content(content)).to_html
     end
     
     # Render content for Textile
     def textile(content)
-      RedCloth.new(content).to_html
+      RedCloth.new(process_content(content)).to_html
+    end
+
+    def process_content(content)
+      data = content.gsub(/^``` ?([^\r\n]+)?\r?\n(.+?)\r?\n```\r?$/m) do
+        if !$1.to_s.empty?
+          "<pre class=\"code-#{$1.strip}\">#{$2}</pre>"
+        else
+          "<pre>#{$2}</pre>"
+        end
+      end
+      data
     end
   end
 end
--- spec/markup_spec.rb
+++ spec/markup_spec.rb
@@ -12,4 +12,12 @@ describe 'Docify::Markup' do
   it 'should render Textile' do
     Docify::Markup.textile(fixture('README.textile')).should == fixture('README.textile.html')
   end
+
+  context 'with flavored content' do
+    it 'should render pre with language class' do
+      doc = Docify::Markup.markdown(fixture('flavored.md'))
+      doc.include?('<pre class="code-lang1">').should be_true
+      doc.include?('<pre class="code-lang2">').should be_true
+    end
+  end
 end
